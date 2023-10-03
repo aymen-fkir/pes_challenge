55df1194cba205e78410eed7636a8784027a0f93
Aymen <aymenfkir23@gmail.com>
2023-10-03 19:41:51 +0100

    security managment

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
 from flask import Flask ,render_template,request
 
 from pymongo import MongoClient
+import hashlib
+
+
+
+
+
+def fetch_by_username(username):
+    client = MongoClient(host="localhost",port=27017)
+    db = client["Energy"]
+    collection = db["users"]
+    user = collection.find_one(username)
+    client.close()
+    return user
+
+def check_username_unique(username):
+    client = MongoClient(host="localhost",port=27017)
+    db = client["Energy"]
+    collection = db["users"]
+    data = collection.find({"username":username})
+    client.close()
+    return data.count()
 
 def insert_to_db(data):
     client = MongoClient(host="localhost",port=27017)
@@ -26,9 +47,12 @@ def check_admin(data):
         return result
 
 def fetch_db(data):
+    sha256 = hashlib.sha256()
     client = MongoClient(host="localhost",port=27017)
     db = client["Energy"]
     collection = db["users"]
+    sha256.update(data["password"].encode('utf-8'))
+    data["password"] = sha256.hexdigest()
     result = collection.find_one(data)
     if result == None:
         return 401
@@ -78,18 +102,29 @@ def signup():
 #for creating account
 @app.route("/create_account",methods=['POST'])
 def create_account():
+    sha256 = hashlib.sha256()
     form = request.form
+    sha256 = hashlib.sha256()
     if form["ver_password"] == form["password"]:
-        data = {"username":form.get("username"),"email":form.get("email"),"password":form.get("password"),"energy_consumtion":0,"cost":0,"polution":0,"region":"east cost"}
+        sha256.update(form["password"].encode('utf-8'))
+        hashed_data = sha256.hexdigest()
+
+        data = {"username":form["username"],"email":form["email"],"password":hashed_data,"energy_consumtion":0,"cost":0,"polution":0,"region":"east cost"}
+        verification_user = check_username_unique(data["username"])
+        if verification_user!=0:
+            return app.redirect("/signup")
+        
         code = insert_to_db(data)
         if code == 200:
             return app.redirect("/")
     return app.redirect("/signup")
 
 # for user page
+
+
 @app.route("/profile/<username>")
 def userinerface(username):
-    profile = fetch_db({"username":username})
+    profile = fetch_by_username({"username":username})
     return render_template("userpage.html",profile=profile)
 
 if __name__ == "__main__":
